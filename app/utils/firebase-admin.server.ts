aa2aede2ddb13f30bd103b4d896d15a8363de1d9
Rickard Lindahl <mail@rickardlindahl.com>
2022-05-25 22:51:21 +0200

    Rename firebase admin file

--- app/utils/firebase-admin.server.ts
+++ app/utils/firebase-admin.server.ts
@@ -1,10 +1,12 @@
 import type { App, AppOptions } from "firebase-admin/app";
 import { applicationDefault, getApp, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import { firebaseConfig } from "~/firebase.config";
+import { getFirebaseConfig } from "~/firebase.config";
 
 const getAppOptions = (isDevelopment: boolean): AppOptions =>
-  isDevelopment ? { projectId: firebaseConfig.projectId } : { credential: applicationDefault() };
+  isDevelopment
+    ? { projectId: getFirebaseConfig(process.env.USE_FIREBASE_EMULATOR === "true").projectId }
+    : { credential: applicationDefault() };
 
 export const app: App =
   getApps().length === 0 ? initializeApp(getAppOptions(process.env.NODE_ENV === "development")) : getApp();
