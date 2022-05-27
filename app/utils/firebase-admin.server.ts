46a8a36530cc0c77237bb41dc9f39b6580e7a091
Rickard Lindahl <mail@rickardlindahl.com>
2022-05-27 16:31:22 +0200

    Use env which are part of service account json

--- app/utils/firebase-admin.server.ts
+++ app/utils/firebase-admin.server.ts
@@ -1,14 +1,29 @@
-import { firestore } from "firebase-admin";
+import { credential, firestore } from "firebase-admin";
 import type { App, AppOptions } from "firebase-admin/app";
-import { applicationDefault, getApp, getApps, initializeApp } from "firebase-admin/app";
+import { getApp, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirebaseConfig } from "~/firebase.config";
 import { postConverter } from "./converters";
 
+const firebaseClientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+if (!firebaseClientEmail) {
+  throw new Error("FIREBASE_CLIENT_EMAIL must be set");
+}
+
+const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+if (!firebasePrivateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY must be set");
+}
+
 const getAppOptions = (isDevelopment: boolean): AppOptions =>
   isDevelopment
     ? { projectId: getFirebaseConfig(process.env.USE_FIREBASE_EMULATOR === "true").projectId }
-    : { credential: applicationDefault() };
+    : {
+        credential: credential.cert({
+          clientEmail: firebaseClientEmail,
+          privateKey: firebasePrivateKey.replace(/\\n/g, "\n"),
+        }),
+      };
 
 export const app: App =
   getApps().length === 0 ? initializeApp(getAppOptions(process.env.NODE_ENV === "development")) : getApp();
