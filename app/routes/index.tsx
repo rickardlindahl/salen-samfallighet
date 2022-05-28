f8c9a4d597d76e664d8d7f3ae28e1f9d89a8d734
Rickard Lindahl <mail@rickardlindahl.com>
2022-05-28 07:13:43 +0200

    Update Loader in index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,27 @@
 import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import type { User } from "~/types/user";
 import { getUserFromSession, requireUserSession } from "~/utils/session.server";
 
-export const loader: LoaderFunction = async ({ request }): Promise<User> => {
+interface LoaderData {
+  user: User;
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
   const session = await requireUserSession(request);
 
-  return getUserFromSession(session);
+  const user = await getUserFromSession(session);
+  return json<LoaderData>({ user });
 };
 
 const Index = () => {
-  const { email } = useLoaderData();
+  const { user } = useLoaderData() as LoaderData;
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      {email && <p>Hello, {email}</p>}
+      {user && <p>Hello, {user.email}</p>}
       <Link to="/posts">Posts</Link>
     </div>
   );
