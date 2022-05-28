b77ce86ac6f0d95c820053589e83f0dfcb357103
Rickard Lindahl <mail@rickardlindahl.com>
2022-05-28 06:58:35 +0200

    Remove Outlet from index file

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
 import type { LoaderFunction } from "@remix-run/node";
-import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import type { User } from "~/types/user";
 import { getUserFromSession, requireUserSession } from "~/utils/session.server";
 
@@ -17,7 +17,6 @@ const Index = () => {
       <h1>Welcome to Remix</h1>
       {email && <p>Hello, {email}</p>}
       <Link to="/posts">Posts</Link>
-      <Outlet />
     </div>
   );
 };
