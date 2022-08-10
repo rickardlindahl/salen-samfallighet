9c2c1ebd494ae69557725debb6d78bb95f0e9206
Rickard Lindahl <mail@rickardlindahl.com>
2022-08-10 14:41:13 +0200

    Update page title

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Salen Samfällighetsförening",
   viewport: "width=device-width,initial-scale=1",
 });
 
